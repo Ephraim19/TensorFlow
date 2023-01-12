b8c4a1dbaa755f06e19dcafea22cbb3f87792925
Ephraim19 <machayoephraim@gmail.com>
2023-01-12 08:12:06 +0300

    Added a callback to terminate the program when the accuracy is above 0.9

--- computerVisionML.py
+++ computerVisionML.py
@@ -1,19 +1,32 @@
 import tensorflow as tf
 
+#Adding a callback to stop the program when we get the least loss
+
+class MyCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self,epochs,logs={}):
+        if(logs.get('accuracy')>0.9):
+            self.model.stop_training = True
+
+callbacks = MyCallback()
+
 mnist = tf.keras.datasets.fashion_mnist
 (training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 
 training_images = training_images / 255.0
 test_images = test_images / 255.0
 
-model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), 
-                                    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
                                     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
-model.compile(optimizer = tf.keras.optimizers.Adam(),
-              loss = 'sparse_categorical_crossentropy',
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(training_images, training_labels, epochs=5)
+model.fit(training_images, training_labels, epochs=15,callbacks= [callbacks])
 
-model.evaluate(test_images,test_labels)
+#model.evaluate(test_images, test_labels)
+
+#classifications = model.predict(test_images)
+
+#print(classifications[0])
